d2ed1afee220104758d099e1ca7bd26ee190127f
☀️ <sumeya4hmed@gmail.com>
2025-05-01 23:44:27 +0100

    allow vercel url in CORS whitelist

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,12 +3,23 @@ import cors from "cors";
 import morgan from "morgan";
 import scrapeRouter from "./routes/pdf-scraper";
 
+const CLIENT_PORT = process.env.CLIENT_PORT || 5173;
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
+
 const app = express();
 
 // MIDDLEWARE
 
 // cors - needs to allow url making the request or server will block it
-app.use(cors({ origin: "http://localhost:5173" }));
+// need to check if there is better way to do this (best practices for security)
+// for now, just allow localhost and vercel
+app.use(
+  cors({
+    origin: [
+      `http://localhost:${CLIENT_PORT}", "https://pdf-waffleizer.vercel.app`,
+    ],
+  })
+);
 // morgan - for logging in terminal
 app.use(morgan("dev"));
 // parsing json
@@ -16,8 +27,6 @@ app.use(express.json());
 // subroute for scraping
 app.use("/scrape", scrapeRouter);
 
-const PORT = process.env.SERVER_PORT;
-
-app.listen(PORT, () =>
-  console.log(`Server listening on http://localhost:${PORT}`)
+app.listen(SERVER_PORT, () =>
+  console.log(`Server listening on http://localhost:${SERVER_PORT}`)
 );
