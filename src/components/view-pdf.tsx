c258a6538f1a78df008a53b14bbd89a3975dace7
☀️ <sumeya4hmed@gmail.com>
2025-05-01 20:17:44 +0100

    tailwind typography plugin added for markdown styling

--- src/components/view-pdf.tsx
+++ src/components/view-pdf.tsx
@@ -2,7 +2,7 @@ import PDF from "../assets/pdf.mdx";
 
 export function ViewPDF() {
   return (
-    <section className="w-full max-w-8xl bg-neutral-400 border border-neutral-800 text-neutral-900 rounded-lg px-10 py-8 shadow-sm space-y-4 text-start font-mono text-md">
+    <section className="prose prose-xl w-full bg-neutral-400 border border-neutral-800 text-neutral-900 rounded-lg px-10 py-8 shadow-sm space-y-4 text-start font-mono ">
       <PDF />
     </section>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
+import mdx from "@mdx-js/rollup";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import mdx from "@mdx-js/rollup";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), mdx()],
+  plugins: [mdx(), react(), tailwindcss()],
 });
